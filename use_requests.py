7f52b6a6bee7dd52f68cc9abc1a71292c385d13c
iboyz <kiki.lukmanhakim@gmail.com>
2021-08-01 11:32:00 +0700

    penggunaan package external dengan requests

--- use_requests.py
+++ use_requests.py
@@ -1,11 +1,11 @@
 import requests
 
-url = 'https://det ik.com'
+url = 'https://detik.com'
 try:
     response = requests.get(url)
     if response.status_code == 200:
-        print(f'success! Response= {response.status_ccode}')
-        #print(f'content{response.text}')
+        print(f'success! Response= {response.status_code}')
+        print(f'content{response.text}')
     else:
         print(f'Woops the page you looking unavailable or encounter some errors {response.status_code}')
 except Exception as e:
